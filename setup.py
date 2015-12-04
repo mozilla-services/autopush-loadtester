b5fa635e84f3d5d28d5b4b59ad870a8801ca2be6
Ben Bangert <ben@groovie.org>
2015-12-04 09:18:03 -0800

    chore: add basic setup.py for a python project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+from aplt import __version__
+
+setup(
+    name='aplt',
+    version=__version__,
+    description='Autopush Load-Tester',
+    url='http://github.com/mozilla-services/ap-loadtester',
+    author='Ben Bangert',
+    author_email='bbangert@mozilla.com',
+    license='MPL2',
+    packages=['aplt'],
+    zip_safe=False
+)
