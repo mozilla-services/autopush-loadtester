baad6e7150317cf74b21f689671045a9b32116de
Ben Bangert <ben@groovie.org>
2015-12-05 09:07:27 -0800

    test: add more thorough harness tests, and bad scenario integration test

--- aplt/tests/__init__.py
+++ aplt/tests/__init__.py
@@ -1,3 +1,5 @@
+from mock import Mock, patch
+from nose.tools import eq_
 from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 from twisted.trial import unittest
@@ -14,9 +16,10 @@ class TestIntegration(unittest.TestCase):
         # Find the connection pool and shut it down
         from treq._utils import get_global_pool
         pool = get_global_pool()
-        return pool.closeCachedConnections()
+        if pool:
+            return pool.closeCachedConnections()
 
-    def testBasic(self):
+    def test_basic(self):
         import aplt.runner as runner
         h = runner.run_scenario({
             "<websocket_url>": "wss://autopush-dev.stage.mozaws.net/",
@@ -25,3 +28,30 @@ class TestIntegration(unittest.TestCase):
         d = Deferred()
         reactor.callLater(0.5, self._check_done, h, d)
         return d
+
+    def test_bad_load(self):
+        import aplt.runner as runner
+        self.assertRaises(Exception, runner.run_scenario, {
+            "<websocket_url>": "wss://autopush-dev.stage.mozaws.net/",
+            "<scenario_function>": "aplt.scenaribasic",
+        }, run=False)
+
+
+class TestHarness(unittest.TestCase):
+    def _make_harness(self):
+        from aplt.runner import RunnerHarness
+        return RunnerHarness("wss://autopush-dev.stage.mozaws.net/")
+
+    def test_no_waiting_processors(self):
+        h = self._make_harness()
+        mock_client = Mock()
+        h.add_client(mock_client)
+        eq_(mock_client.sendClose.called, True)
+
+    @patch("aplt.runner.connectWS")
+    def test_remove_client_with_waiting_processors(self, mock_connect):
+        h = self._make_harness()
+        h._connect_waiters.append(Mock())
+        mock_client = Mock()
+        h.remove_client(mock_client)
+        eq_(mock_connect.called, True)
