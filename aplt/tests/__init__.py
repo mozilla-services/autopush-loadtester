f461dfd6f28e582b05ad687d937e4911e3e1c39d
Ben Bangert <ben@groovie.org>
2015-12-04 19:12:36 -0800

    fix: update basic integration so it passes

--- aplt/tests/__init__.py
+++ aplt/tests/__init__.py
@@ -1,20 +1,27 @@
-from mock import Mock
 from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 from twisted.trial import unittest
 
 
 class TestIntegration(unittest.TestCase):
-    def _check_done(self, d):
-        d.callback(True)
+    def _check_done(self, harness, d):
+        if not harness._processors:
+            d.callback(True)
+        else:
+            reactor.callLater(0.5, self._check_done, harness, d)
+
+    def tearDown(self):
+        # Find the connection pool and shut it down
+        from treq._utils import get_global_pool
+        pool = get_global_pool()
+        return pool.closeCachedConnections()
 
     def testBasic(self):
         import aplt.runner as runner
-        runner.RunnerHarness.reactor = Mock()
-        runner.run_scenario({
+        h = runner.run_scenario({
             "<websocket_url>": "wss://autopush-dev.stage.mozaws.net/",
             "<scenario_function>": "aplt.scenarios:basic",
-        })
+        }, run=False)
         d = Deferred()
-        reactor.callLater(15, self._check_done, d)
+        reactor.callLater(0.5, self._check_done, h, d)
         return d
